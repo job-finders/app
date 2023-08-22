47768246a0e85446e46469ab3dc5083a0564a7ff
freelancing-solutions <mobiusndou@gmail.com>
2023-08-22 15:42:41 +0200

    refactored job search

--- app.py
+++ app.py
@@ -7,5 +7,5 @@ app = create_app(config=config_instance())
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8000, debug=True, extra_files=['src', 'templates', 'static'])
+    app.run(host='0.0.0.0', port=8080, debug=True, extra_files=['src', 'templates', 'static'])
 
