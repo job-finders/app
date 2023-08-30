976d5637fe3b2d6dbe62160789cc50da873d3953
freelancing-solutions <mobiusndou@gmail.com>
2023-08-30 21:54:51 +0200

    refactored code

--- src/main/__init__.py
+++ src/main/__init__.py
@@ -3,6 +3,7 @@ from src.scrappers import JunctionScrapper, CareerScrapper, Scrapper
 from src.utils import template_folder, static_folder, format_title, format_description
 from src.controllers import StorageController
 
+# initializing models and controllers
 storage_controller = StorageController()
 scrapper = Scrapper()
 junction_scrapper = JunctionScrapper(scrapper=scrapper)
@@ -19,17 +20,23 @@ def create_app(config):
     app.template_folder = template_folder()
     app.static_folder = static_folder()
     app.config['SECRET_KEY'] = config.SECRET_KEY
-    app.config['BASE_URL'] = "https://rental-manager.site"
+    app.config['BASE_URL'] = "https://jobfinders.site"
 
     with app.app_context():
+        # initialization
         storage_controller.init_app(app=app)
         junction_scrapper.init_app(app=app)
         career_scrapper.init_app(app=app)
+
+        # importing routes
         from src.routes.home import home_route
         from src.routes.seo import seo_route
+
+        # registering routes
         app.register_blueprint(home_route)
         app.register_blueprint(seo_route)
 
+        # registering filters
         app.jinja_env.filters['title'] = format_title
         app.jinja_env.filters['description'] = format_description
     return app
