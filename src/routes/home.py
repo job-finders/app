334ee31c01d88d57e6b0dc9883759d1b1bb24fb3
freelancing-solutions <mobiusndou@gmail.com>
2023-09-07 08:13:59 +0200

    search bar updated

--- src/routes/home.py
+++ src/routes/home.py
@@ -71,6 +71,84 @@ async def create_context(search_term: str, page: int = 1, per_page: int = 10):
     return render_template('index.html', **context)
 
 
+async def create_search_context(search_term: str, page: int = 1, per_page: int = 10):
+    """
+    Create common context for jobs with paged results.
+
+    :param search_term: The search term for job listings.
+    :param page: The page number of results to display (default is 1).
+    :param per_page: The number of job listings per page (default is 10).
+    :return: The context for rendering the template.
+    """
+    # Filter jobs based on the search term matching any string field
+    job_list = [job for job in scrapper.jobs.values() if
+                search_term_matches_any_field(job, search_term)]
+
+    # Calculate the start and end indices for the current page
+    start_idx = (page - 1) * per_page
+    end_idx = start_idx + per_page
+
+    # Slice the job list to get the jobs for the current page
+    job_list_page = job_list[start_idx:end_idx]
+
+    search_terms: list[str] = scrapper.search_terms
+
+    seo = await create_tags(search_term=search_term)
+    if search_term in search_terms:
+        current_index: int = search_terms.index(search_term)
+        previous_term: str = search_terms[current_index - 1] if current_index > 0 else search_terms[
+            len(search_terms) - 1]
+        next_term: str = search_terms[current_index + 1] if current_index < len(search_terms) - 1 else search_terms[0]
+    else:
+        current_index = 0
+        previous_term = "programming"
+        next_term = "information-technolodgy"
+
+    context = dict(
+        term=search_term,
+        previous_term=previous_term,
+        next_term=next_term,
+        job_list=job_list_page,  # Use the sliced job list for the current page
+        search_terms=search_terms,
+        seo=seo,
+        current_page=page,  # Include the current page number in the context
+        total_pages=math.ceil(len(job_list) / per_page)  # Calculate the total number of pages
+    )
+
+    return render_template('index.html', **context)
+
+
+def search_term_matches_any_field(job: Job, search_term: str):
+    # Check if the search term matches any string field of the job
+    if field_contains_search_term(field=job.location, search_term=search_term):
+        return True
+    if field_contains_search_term(field=job.search_term, search_term=search_term):
+        return True
+    if field_contains_search_term(field=job.title, search_term=search_term):
+        return True
+    if field_contains_search_term(field=job.job_link, search_term=search_term):
+        return True
+    if field_contains_search_term(field=job.description, search_term=search_term):
+        return True
+    if field_contains_search_term(field=job.company_name, search_term=search_term):
+        return True
+    if field_contains_search_term(field=job.desired_skills, search_term=search_term):
+        return True
+    return False
+
+
+def field_contains_search_term(field: str | list, search_term: str):
+    # Check if the field contains the search term
+    if isinstance(field, list):
+        for term in field:
+            if search_term.lower() in term.lower():
+                return True
+
+    if isinstance(field, str):
+        return search_term.lower() in field.lower()
+    return False
+
+
 async def not_found(search_term: str):
     error_message = f"Unable to retrieve job listings for :  {search_term}"
     status = "404 Not Found"
@@ -95,15 +173,15 @@ async def get_home():
 @home_route.get('/jobs/<string:search_term>')
 async def job_search(search_term: str):
     page = int(request.args.get('page', 1))
-    response = await create_context(search_term=search_term, page=page)
+    response = await create_search_context(search_term=search_term, page=page)
     if response is None:
         return await not_found(search_term=search_term)
 
     return response
 
 
-@home_route.get('/job-search/<string:search_term>')
-async def search_bar(search_term: str):
+@home_route.get('/search')
+async def search_bar():
     """
         allows the front page to display a search bar
         which enables our clients to search for jobs using any term
@@ -111,7 +189,16 @@ async def search_bar(search_term: str):
     :param search_term:
     :return:
     """
-    pass
+    search_term = request.args.get('search_term')
+    if search_term is None:
+        return redirect(url_for('home.get_home'), code=302)
+
+    page = int(request.args.get('page', 1))
+    response = await create_search_context(search_term=search_term, page=page)
+    if response is None:
+        return await not_found(search_term=search_term)
+
+    return response
 
 
 @home_route.get('/job/<string:reference>')
@@ -204,10 +291,13 @@ async def email_me(search_term: str):
         created_notification = await notifications_controller.create_notification_email(notification=notifications)
 
         if not created_notification:
-            flash(message="There was a problem adding you to the email list you may already be added please verify your email - also note that we cannot add you to more than one list at a time", category="danger")
+            flash(
+                message="There was a problem adding you to the email list you may already be added please verify your email - also note that we cannot add you to more than one list at a time",
+                category="danger")
             return redirect(url_for('home.get_home'), code=302)
 
-        email_sent = await notifications_controller.send_notification_verification_email(notification=created_notification)
+        email_sent = await notifications_controller.send_notification_verification_email(
+            notification=created_notification)
 
     except ValidationError as e:
         flash(message="There was a problem creating your email alert please try again later", category="danger")
@@ -245,4 +335,3 @@ async def verify_email(verification_id: str):
 
     flash(message="Unfortunately we could not verify your email address please try again", category="danger")
     return redirect(url_for('home.get_home'), code=302)
-
