aa2075cc572d9678e72a488c6de6a48a1a606a2c
freelancing-solutions <mobiusndou@gmail.com>
2023-09-08 17:19:25 +0200

    added articles

--- src/routes/blog.py
+++ src/routes/blog.py
@@ -20,3 +20,30 @@ async def get_blog():
     context = dict(seo=seo, term="blog")
     return render_template("blog/blog.html", **context)
 
+
+@blog_route.get('/blog/resume-writing-tips')
+async def resume_writing_tips():
+    seo = create_tags(search_term="Resume Writing Tips")
+    context = dict(seo=seo, term="Resume Writing Tips")
+    return render_template("blog/resume/resume_writing_tips.html", **context)
+
+
+@blog_route.get('/blog/job-search-strategies')
+async def job_search_strategies():
+    seo = create_tags(search_term="Job Search Strategies")
+    context = dict(seo=seo, term="Job Search Strategies")
+    return render_template("blog/job_search/index.html", **context)
+
+
+@blog_route.get('/blog/interview-strategies')
+async def interview_strategies():
+    seo = create_tags(search_term="Interview Strategies")
+    context = dict(seo=seo, term="Interview Strategies")
+    return render_template("blog/interview/index.html", **context)
+
+
+@blog_route.get('/blog/remote-work-freelancing')
+async def freelancing():
+    seo = create_tags(search_term="Remote Work and Freelancing")
+    context = dict(seo=seo, term="Remote Work and Freelancing")
+    return render_template("blog/freelancing/index.html", **context)
