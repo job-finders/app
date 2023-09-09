9bf3ac1dbf99dce3ae488ffd1df5335941733563
freelancing-solutions <mobiusndou@gmail.com>
2023-09-09 14:14:19 +0200

    Refactored Blog

--- src/routes/blog.py
+++ src/routes/blog.py
@@ -1,77 +1,38 @@
-from flask import Blueprint, render_template, send_from_directory
+from flask import Blueprint, render_template
 
 from src.database.models.seo import create_tags
-from src.database.models import Job, SEO
 from src.logger import init_logger
-from src.main import scrapper
-from src.utils import static_folder, format_title
 
 blog_route = Blueprint('blog', __name__)
 blog_logger = init_logger()
 
 
-@blog_route.get('/blog')
-async def get_blog():
+@blog_route.get('/blog/<string:topic>')
+async def get_blog(topic):
     """
-        will return blog
-    :return:
+    will return blog for the specified topic
+    :param topic: The topic of the blog
+    :return: The rendered template for the specified topic
     """
-    seo = create_tags(search_term="Blog")
-    context = dict(seo=seo, term="blog")
-    return render_template("blog/blog.html", **context)
+    # Define a dictionary to map topics to search terms and template paths
+    topic_mappings = {
+        "index": ("Jobfinders Blog Articles", "blog/blog.html"),
+        "resume-writing-tips": ("Resume Writing Tips", "blog/resume/index.html"),
+        "job-search-strategies": ("Job Search Strategies", "blog/job_search/index.html"),
+        "interview-strategies": ("Interview Strategies", "blog/interview/index.html"),
+        "remote-work-freelancing": ("Remote Work and Freelancing", "blog/freelancing/index.html"),
+        "job-application-tips": ("Job Application Tips", "blog/job_application/index.html"),
+        "workplace-tips": ("Workplace Tips", "blog/workplace_tips/index.html"),
+        "job-market-research": ("Job Market Research", "blog/job_market/index.html"),
+        "education": ("Education and Training", "blog/education/index.html"),
+    }
 
-
-@blog_route.get('/blog/resume-writing-tips')
-async def resume_writing_tips():
-    seo = create_tags(search_term="Resume Writing Tips")
-    context = dict(seo=seo, term="Resume Writing Tips")
-    return render_template("blog/resume/index.html", **context)
-
-
-@blog_route.get('/blog/job-search-strategies')
-async def job_search_strategies():
-    seo = create_tags(search_term="Job Search Strategies")
-    context = dict(seo=seo, term="Job Search Strategies")
-    return render_template("blog/job_search/index.html", **context)
-
-
-@blog_route.get('/blog/interview-strategies')
-async def interview_strategies():
-    seo = create_tags(search_term="Interview Strategies")
-    context = dict(seo=seo, term="Interview Strategies")
-    return render_template("blog/interview/index.html", **context)
-
-
-@blog_route.get('/blog/remote-work-freelancing')
-async def freelancing():
-    seo = create_tags(search_term="Remote Work and Freelancing")
-    context = dict(seo=seo, term="Remote Work and Freelancing")
-    return render_template("blog/freelancing/index.html", **context)
-
-
-@blog_route.get('/blog/job-application-tips')
-async def job_application():
-    seo = create_tags(search_term="Job Application Tips")
-    context = dict(seo=seo, term="Job Application Tips")
-    return render_template("blog/job_application/index.html", **context)
-
-
-@blog_route.get('/blog/workplace-tips')
-async def workplace_tips():
-    seo = create_tags(search_term="Workplace Tips")
-    context = dict(seo=seo, term="Workplace Tips")
-    return render_template("blog/workplace_tips/index.html", **context)
-
-
-@blog_route.get('/blog/job-market-research')
-async def job_market_research():
-    seo = create_tags(search_term="Job Market Research")
-    context = dict(seo=seo, term="Job Market Research")
-    return render_template("blog/job_market/index.html", **context)
-
-
-@blog_route.get('/blog/education')
-async def education():
-    seo = create_tags(search_term="Education and Training")
-    context = dict(seo=seo, term="Education and Training")
-    return render_template("blog/education/index.html", **context)
+    if topic in topic_mappings:
+        search_term, template_path = topic_mappings[topic]
+        seo = await create_tags(search_term=search_term)
+        context = dict(seo=seo, term=search_term)
+        return render_template(template_path, **context)
+    else:
+        # Handle invalid topic here, e.g., return a 404 page
+        error = dict(title="Error Page not Found", message="Error finding that blog page")
+        return render_template("error.html")
