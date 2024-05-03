270638d077d90140b1643b35af3b8fe21a0f95ba
freelancing-solutions <mobiusndou@gmail.com>
2024-05-03 19:08:52 +0200

    adds text

--- app.py
+++ app.py
@@ -6,5 +6,5 @@ app = create_app(config=config_instance())
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080, debug=True, extra_files=['src', 'templates', 'static'])
+    app.run(host='0.0.0.0', port=8084, debug=True, extra_files=['src', 'templates', 'static'])
 
