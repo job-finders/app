be38cdab93776b34017bd0db7f22cc5affedded6
mobius-crypt <mobiusndou@gmail.com>
2025-07-24 11:28:51 +0200

    added hashnode agent interface

--- src/services/hashnode/__init__.py
+++ src/services/hashnode/__init__.py
@@ -1,2 +1,3 @@
 from .hashnode_agent_interface import HashnodeAgentCommandRegistry
 from .hashnode_client import HashnodeService, UpdatePostInput, CreatePostInput
+from .hashnode_agent_interface import HashnodeAgentInterface
